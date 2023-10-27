2a65587aa353c87e3f5a6644b3537fcb54235ea5
chahatjaink <chahatjaink@gmail.com>
2023-10-27 14:23:40 +0530

    feat: react startup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+}
+
+
+function App() {
+  return (
+    <div>
+      <main>
+        <Header />
+        <h2>Start APP</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
